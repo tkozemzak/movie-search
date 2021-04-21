d86d4657fd4b34a02ed962d2d0854b179591a6e0
tkozemzak <timkozemzak@gmail.com>
2021-04-21 14:44:53 -0700

    add counter for bucket testing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">Hello</header>
+      <header className="App-header">
+        <button onClick={() => setCount(count + 1)}>Add</button>
+        <p>{count}</p>
+      </header>
     </div>
   );
 }
