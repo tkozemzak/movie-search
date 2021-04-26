53ac34213d097cf8db20e4e1956a2ccb836f188f
tkozemzak <timkozemzak@gmail.com>
2021-04-26 16:13:43 -0700

    update cloudfront.tf with cache behavior config

--- infrastructure/cloudfront.tf
+++ infrastructure/cloudfront.tf
@@ -20,6 +20,21 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     default_route_object = "index.html"
 
     default_cache_behavior {
+        allowed_methods = [ "GET", "HEAD", "OPTIONS" ]
+        cached_methods = [ "GET", "HEAD" ]
+        target_origin_id = "default-origin"
+        viewer_protocol_policy = "redirect-to-https"
+        compress = true
+        min_ttl = 0
+        default_ttl = 300
+        max_ttl = 300
         
+
+        forwarded_values {
+          query_string = true
+          cookies {
+              forward = "all"
+          }
+        }
     }
 }
