7150a1645db70a6add9826655354ee6ffd793bc7
tkozemzak <timkozemzak@gmail.com>
2021-04-26 20:16:41 -0700

    configure mobile menu toggle

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../../logo.png';
 import './Header.scss';
 
+const HEADER_LIST = [
+  {
+    id: 1,
+    iconClass: 'fas fa-film',
+    name: 'Now Playing',
+    type: 'now_playing'
+  },
+  {
+    id: 2,
+    iconClass: 'fas fa-film',
+    name: 'Now Playing',
+    type: 'now_playing'
+  }
+];
+
 const Header = () => {
+  let [navClass, setNavClass] = useState(false);
+  let [menuClass, setMenuClass] = useState(false);
+
+  const toggleMenu = () => {
+    menuClass = !menuClass;
+    setMenuClass(menuClass);
+
+    navClass = !navClass;
+    setNavClass(navClass);
+
+    navClass ? document.body.classList.add('header-nav-open') : document.body.classList.remove('header-nav-open');
+  };
+
   return (
     <>
       <div className="header-nav-wrapper">
@@ -11,12 +39,18 @@ const Header = () => {
           <div className="header-image">
             <img src={logo} alt="" />
           </div>
-          <div className="header-menu-toggle" id="header-mobile-menu">
+          <div
+            onClick={() => {
+              toggleMenu();
+            }}
+            className={`${menuClass ? 'header-menu-toggle is-active' : 'header-menu-toggle'}`}
+            id="header-mobile-menu"
+          >
             <span className="bar"></span>
             <span className="bar"></span>
             <span className="bar"></span>
           </div>
-          <ul className="header-nav">
+          <ul className={`${navClass ? 'header-nav header-mobile-nav' : 'header-nav'}`}>
             <li className="header-nav-item">Now Playing</li>
             <li className="header-nav-item">New Movies</li>
             <input className="search-input" type="text" placeholder="Search for a movie" />
