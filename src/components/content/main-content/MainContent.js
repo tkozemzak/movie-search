e122d033dc793b08e830b5c05dd8afd00ab1f231
tkozemzak <timkozemzak@gmail.com>
2021-04-26 21:21:16 -0700

    cleanup

---
+++ src/components/content/main-content/MainContent.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const MainContent = () => {
+  return (
+    <div className="main-content">
+      {/* slideshow component */}
+      <div className="grid-movie-title">
+        <div className="movieType">Now Playing</div>
+        <div className="paginate">Paginate</div>
+      </div>
+      {/* grid component */}
+    </div>
+  );
+};
+
+export default MainContent;
---
+++ src/components/main/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import MainContent from '../content/main-content/MainContent';
+
+const Main = () => {
+  return (
+    <div className="main">
+      <h1>Main</h1>
+      <MainContent />
+    </div>
+  );
+};
+
+export default Main;
