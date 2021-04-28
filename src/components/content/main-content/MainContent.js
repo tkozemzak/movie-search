e33e554f6a1de99c190f8467cc8513a24251ee98
tkozemzak <timkozemzak@gmail.com>
2021-04-28 12:06:28 -0700

    create grid component fs

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Slideshow from '../slide-show/Slideshow';
 import './MainContent.scss';
 import Paginate from '../paginate/Paginate';
+import Grid from '../grid/Grid';
 
 const MainContent = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -35,7 +36,7 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      {/* grid component */}
+      <Grid />
     </div>
   );
 };
