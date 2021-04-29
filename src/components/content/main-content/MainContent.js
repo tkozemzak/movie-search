e65a8942e3d1522436a8c714fd357112479764b6
tkozemzak <timkozemzak@gmail.com>
2021-04-29 11:18:00 -0700

    set up redux connection for random images in main content

--- src/components/content/main-content/MainContent.js
+++ src/components/content/main-content/MainContent.js
@@ -1,11 +1,41 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Slideshow from '../slide-show/Slideshow';
 import './MainContent.scss';
 import Paginate from '../paginate/Paginate';
 import Grid from '../grid/Grid';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { IMAGE_URL } from '../../../services/movies.service';
 
-const MainContent = () => {
+const MainContent = ({ list }) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [images, setImages] = useState([]);
+
+  const randomImages = list.sort(() => Math.random() - Math.random()).slice(0, 4);
+
+  useEffect(() => {
+    if (randomImages.length) {
+      const IMAGES = [
+        {
+          id: 1,
+          url: `${IMAGE_URL}/${randomImages[0].backdrop_path}`
+        },
+        {
+          id: 2,
+          url: `${IMAGE_URL}/${randomImages[1].backdrop_path}`
+        },
+        {
+          id: 3,
+          url: `${IMAGE_URL}/${randomImages[2].backdrop_path}`
+        },
+        {
+          id: 4,
+          url: `${IMAGE_URL}/${randomImages[3].backdrop_path}`
+        }
+      ];
+      setImages(IMAGES);
+    }
+  }, []);
 
   const paginate = (type) => {
     if (type === 'prev' && currentPage >= 1) {
@@ -15,7 +45,7 @@ const MainContent = () => {
     }
   };
 
-  const images = [
+  const imagesArray = [
     {
       url: 'https://wallpapercave.com/wp/4KOmgLX.jpg',
       rating: 7.5
@@ -47,9 +77,17 @@ const MainContent = () => {
           <Paginate currentPage={currentPage} totalPages={10} paginate={paginate} />
         </div>
       </div>
-      <Grid images={images} />
+      <Grid images={imagesArray} />
     </div>
   );
 };
 
-export default MainContent;
+MainContent.propTypes = {
+  list: PropTypes.array.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list
+});
+
+export default connect(mapStateToProps, {})(MainContent);
