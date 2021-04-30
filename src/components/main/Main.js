7c49042129d1e03cfa7896d455a99e7ea9e03e46
tkozemzak <timkozemzak@gmail.com>
2021-04-29 17:25:53 -0700

    start on loadOnScroll feature for grid

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,9 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import MainContent from '../content/main-content/MainContent';
 import Spinner from '../spinner/Spinner';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loadMoreMovies } from '../../redux/actions/movies';
 
-const Main = () => {
+const Main = ({ loadMoreMovies, page }) => {
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(page);
+
+  const mainRef = useRef();
+  const bottomLineRef = useRef();
 
   //add 3 second loader until redux store can be populated
   useEffect(() => {
@@ -14,7 +21,35 @@ const Main = () => {
     }, 3000);
   }, []);
 
-  return <div className="main">{loading ? <Spinner /> : <MainContent />}</div>;
+  //redux action for loadmoremovies
+  useEffect(() => {
+    loadMoreMovies('now_playing', currentPage);
+  });
+
+  const handleScroll = () => {
+    const { height } = mainRef.current.getBoundingClientRect();
+    const { top: bottomLineTop } = mainRef.current.getBoundingClientRect();
+    if (bottomLineTop <= height) {
+      //fetch data here
+    }
+  };
+
+  return (
+    <div className="main" ref={mainRef} onScroll={() => handleScroll()}>
+      {loading ? <Spinner /> : <MainContent />}
+      <div ref={bottomLineRef}></div>
+    </div>
+  );
 };
 
-export default Main;
+Main.propTypes = {
+  list: PropTypes.array,
+  page: PropTypes.number,
+  loadMoreMovies: PropTypes.func
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list
+});
+
+export default connect(mapStateToProps, { loadMoreMovies })(Main);
