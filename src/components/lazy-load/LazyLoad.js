bc3c46e5a52fbbb99372077943ad9efb942d9b37
tkozemzak <timkozemzak@gmail.com>
2021-05-03 12:51:31 -0700

    create useEffect logic for observer and setting ref observer

--- src/components/lazy-load/LazyLoad.js
+++ src/components/lazy-load/LazyLoad.js
@@ -1,12 +1,48 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
-import placeHolder from '../../assets/lazy-loader.gif';
 import Spinner from '../spinner/Spinner';
 
 const LazyLoad = ({ src, alt, children, className }) => {
   const [imageSrc, setImageSrc] = useState();
   const [imageRef, setImageRef] = useState();
+
+  useEffect(() => {
+    let observer;
+    let didCancel = false;
+    //check for existing imageRef, imageSrc, and Observer
+    if (imageRef && imageSrc !== src) {
+      if (IntersectionObserver) {
+        //create Observer instance, take entries in callback
+        observer = new IntersectionObserver(
+          (entries) => {
+            //loop through entries
+            entries.forEach((entry) => {
+              if (!didCancel && (entry.intersectionRatio > 0 || entry.isIntersecting)) {
+                //set image source and stop listening to imageRef
+                setImageSrc(src);
+                observer.unobserve(imageRef);
+              }
+            });
+          },
+          {
+            threshold: 0.01,
+            rootMargin: '75%'
+          }
+        );
+        observer.observe(imageRef);
+      } else {
+        setImageSrc(src);
+      }
+    }
+
+    return () => {
+      didCancel = true;
+      if (observer && observer.unobserve) {
+        observer.unobserve(imageRef);
+      }
+    };
+  }, [src, imageSrc, imageRef]);
 
   return (
     <>
@@ -22,6 +58,13 @@ const LazyLoad = ({ src, alt, children, className }) => {
       )}
     </>
   );
+};
+
+LazyLoad.propTypes = {
+  src: PropTypes.string,
+  alt: PropTypes.string,
+  children: PropTypes.any,
+  className: PropTypes.string
 };
 
 export default LazyLoad;
