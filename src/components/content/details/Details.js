13398601f09bf6d177b6714de6da4fdbfddab65e
tkozemzak <timkozemzak@gmail.com>
2021-05-09 13:05:58 -0700

    add styles for Details

--- src/components/content/details/Details.js
+++ src/components/content/details/Details.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Rating from '../rating/Rating';
+import './Details.scss';
 
 const tempImg = 'https://images.pexels.com/photos/1640773/pexels-photo-1640773.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
 
@@ -7,30 +8,27 @@ const Details = () => {
   return (
     <>
       <div className="movie-container">
-        <div className="movie-bg" style={{ backgroundImage: `url(${tempImg})` }}></div>
-        <div className="movie-overlay">
-          <div className="movie-details">
-            <div className="movie-image">
-              <img src={tempImg} alt="" />
-            </div>
-            <div className="movie-body">
-              <div className="movie-overview">
-                <div className="title">
-                  <h2>
-                    Movie Title <span>2020-12-03</span>
-                  </h2>
-                </div>
-                <div className="movie-genres">
-                  <ul className="genres">
-                    <li>Action</li>
-                    <li>Comedy</li>
-                  </ul>
-                </div>
-                <div className="rating">
-                  <Rating className="rating-stars" rating={6.5} totalStars={5} />
-                  &nbsp;
-                  <span>6.5</span> <p>(200) reviews</p>
-                </div>
+        <div className="movie-bg" style={{ backgroundImage: `url(${tempImg}})` }}></div>
+        <div className="movie-overlay"></div>
+        <div className="movie-details">
+          <div className="movie-image">
+            <img src={`${tempImg}`} alt="" />
+          </div>
+          <div className="movie-body">
+            <div className="movie-overview">
+              <div className="title">
+                Title <span>Release Date</span>
+              </div>
+              <div className="movie-genres">
+                <ul className="genres">
+                  <li>Comedy</li>
+                  <li>Action</li>
+                </ul>
+              </div>
+              <div className="rating">
+                <Rating className="rating-stars" rating={6.5} totalStars={5} />
+                &nbsp;
+                <span>6.5</span> <p>(200) reviews</p>
               </div>
             </div>
           </div>
