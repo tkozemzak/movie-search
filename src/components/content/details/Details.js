77e5f7938c587138a4f0d873626cb9ede5193182
tkozemzak <timkozemzak@gmail.com>
2021-05-09 12:57:19 -0700

    create layout for details component

--- src/components/content/details/Details.js
+++ src/components/content/details/Details.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import Rating from '../rating/Rating';
+
+const tempImg = 'https://images.pexels.com/photos/1640773/pexels-photo-1640773.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260';
 
 const Details = () => {
   return (
-    <div>
-      <h1>Details component</h1>
-    </div>
+    <>
+      <div className="movie-container">
+        <div className="movie-bg" style={{ backgroundImage: `url(${tempImg})` }}></div>
+        <div className="movie-overlay">
+          <div className="movie-details">
+            <div className="movie-image">
+              <img src={tempImg} alt="" />
+            </div>
+            <div className="movie-body">
+              <div className="movie-overview">
+                <div className="title">
+                  <h2>
+                    Movie Title <span>2020-12-03</span>
+                  </h2>
+                </div>
+                <div className="movie-genres">
+                  <ul className="genres">
+                    <li>Action</li>
+                    <li>Comedy</li>
+                  </ul>
+                </div>
+                <div className="rating">
+                  <Rating className="rating-stars" rating={6.5} totalStars={5} />
+                  &nbsp;
+                  <span>6.5</span> <p>(200) reviews</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
